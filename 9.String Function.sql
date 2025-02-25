ebf52d27d2630e31d0cbf60249a2759ffd4f0b18
andromedalanglangjagad <langjagad25@gmail.com>
2025-02-25 21:43:00 +0700

    ok

---
+++ 9.String Function.sql
@@ -0,0 +1,44 @@
+-- String Functions
+USE parks_and_recreation;
+
+
+SELECT LENGTH('SKYFALL');
+
+SELECT first_name, LENGTH(first_name)
+FROM employee_demographics
+ORDER BY 2;
+
+
+SELECT UPPER('sky');
+SELECT LOWER('sky');
+
+SELECT first_name, UPPER(first_name)
+FROM employee_demographics;
+
+
+SELECT RTRIM('           strength             ');
+
+
+SELECT first_name, LEFT(first_name, 4)
+FROM employee_demographics;
+
+SELECT first_name, RIGHT(first_name, 4),
+SUBSTRING(first_name, 3, 2),
+birth_date,
+SUBSTRING(birth_date,6,2) AS Month
+FROM employee_demographics;
+
+
+SELECT first_name, REPLACE(first_name, 'A', 'z')
+FROM employee_demographics;
+
+
+SELECT LOCATE('x', 'Alexander');
+
+
+SELECT first_name, LOCATE('ril', first_name)
+FROM employee_demographics;
+
+SELECT first_name, last_name,
+CONCAT(first_name, ' ', last_name) AS full_name
+FROM employee_demographics;
