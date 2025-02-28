f3bea928b4f6cea7fecd30dcbfe3ea44824b538a
andromedalanglangjagad <langjagad25@gmail.com>
2025-02-28 19:15:45 +0700

    ok

---
+++ 13. CTEs.sql
@@ -0,0 +1,32 @@
+-- CTEs
+USE parks_and_recreation;
+
+WITH CTE_Example AS 
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example
+;
+
+
+SELECT AVG(avg_sal)
+FROM (
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+) example_subquery
+;
+
+
+SELECT AVG(avg_sal)
+FROM CTE_Example;
+
+
+
