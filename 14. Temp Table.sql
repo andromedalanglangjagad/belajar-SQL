62766c566eed33b321986f07c38ceda898536d06
Andromeda <andro55lang@google.com>
2025-03-12 20:32:09 +0700

    ok

---
+++ 14. Temp Table.sql
@@ -0,0 +1,34 @@
+-- temporary table
+USE parks_and_recreation;
+
+
+CREATE TEMPORARY TABLE temp_table
+(first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(100)
+);
+
+
+SELECT *
+FROM temp_table;
+
+
+INSERT INTO temp_table
+VALUES('alex','freberg','Lord of the Rings : The Two Towers');
+
+SELECT *
+FROM temp_table;
+
+
+SELECT * 
+FROM employee_salary;
+
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+
+SELECT *
+FROM salary_over_50k;
+
