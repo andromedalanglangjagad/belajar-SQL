cce1c27c3c08d45b417cb102aba84b23ad61badc
andromedalanglangjagad <langjagad25@gmail.com>
2025-03-15 08:00:26 +0700

    ok

--- 16. Triggers and Events.sql
+++ 16. Triggers and Events.sql
@@ -172,7 +172,7 @@ DELIMITER ;
 DELIMITER $$
 CREATE EVENT ResetSalaryForExiterEmployees
 ON SCHEDULE EVERY 1 MONTH
-STARTS TIMESTAMP(CONCAT(YEAR(CURDATE())), '-' +)
+STARTS TIMESTAMP(CONCAT(YEAR(CURDATE())), '-', MONTH(CURDATE()) +)
 	UPDATE employee_salary
     SET salary = NULL
 	WHERE employee_id NOT IN (SELECT employee_id FROM employee_demographics);
