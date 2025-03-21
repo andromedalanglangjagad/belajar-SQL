249407f50d6493c9c9764d804439a9b455ad15db
andromedalanglangjagad <langjagad25@gmail.com>
2025-03-21 23:10:07 +0700

    ok

--- 17. DATA CLEANING (FP).sql
+++ 17. DATA CLEANING (FP).sql
@@ -72,5 +72,27 @@ FROM duplicate_cte
 WHERE ROW_NUM > 1;
 
 
+CREATE TABLE `layoffs_stagging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+SELECT *
+FROM layoffs_stagging2
+;
+
+
+
+
+
 
 
