2a00feb2e8af4dd3fadec7be5387b70afe3b01a6
andromedalanglangjagad <langjagad25@gmail.com>
2025-03-28 17:49:41 +0700

    ok

--- 17. DATA CLEANING (FP).sql
+++ 17. DATA CLEANING (FP).sql
@@ -183,6 +183,9 @@ WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL
 ;
 
+UPDATE layoffs_stagging2
+SET industry = NULL
+WHERE industry = '';
 
 SELECT *
 FROM layoffs_stagging2
@@ -190,7 +193,26 @@ WHERE industry IS NULL
 OR industry = ''
 ;
 
+SELECT *
+FROM layoffs_stagging2
+WHERE company LIKE '%bally%'
+;
 
+SELECT t1.industry, t2.industry
+FROM layoffs_stagging2 t1
+JOIN layoffs_stagging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL
+    
+;
+
+UPDATE layoffs_stagging2 t1
+JOIN layoffs_stagging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
 
 
 
