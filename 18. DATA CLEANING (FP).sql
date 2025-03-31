af98448431d0047195c8fc256738e35d1f788309
andromedalanglangjagad <langjagad25@gmail.com>
2025-03-31 20:11:50 +0700

    ok

---
+++ 18. DATA CLEANING (FP).sql
@@ -0,0 +1,40 @@
+-- Exploratory Data Analysis
+
+SELECT *
+FROM layoffs_stagging2
+;
+
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_stagging2
+;
+
+
+SELECT *
+FROM layoffs_stagging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC
+;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_stagging2
+GROUP BY company
+ORDER BY 2 DESC
+;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_stagging2
+;
+
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_stagging2
+GROUP BY country
+ORDER BY 2 DESC
+;
+
+SELECT *
+FROM layoffs_stagging2
+;
+
+
